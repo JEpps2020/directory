9a0cfb9456f4b0c4b675278d15da2850a6b80e44
Jasmine Epps <epps.jasmine@gmail.com>
2020-06-16 22:43:36 -0400

    updated app.js, employee.json, search results compmnent

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import React from "react";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
 import SearchBox from "./components/SearchBox";
+import SearchResults from "./components/SearchResults";
+import Container from "./components/Container";
 import employees from "./employees.json";
 
 
 class App extends React.Component {
 
   state = {
-    employees
+    employees: []
     };
 
+    componentDidMount() {
+        this.setState({ employees: employees });
+    }
+  
     render() {
       return (
         <Wrapper>
           <Header>Employee Header</Header>
-          <SearchBox></SearchBox>
+          <Container>
+        <SearchBox>
+        </SearchBox> 
+        
+        <SearchResults employees={this.state.employees} />     
+        </Container>
         </Wrapper>
       );
     }
